b06e14ecbce26c96480a5c3132aa57b716977a23
martynas3113 <martynas3113@users.noreply.github.com>
2020-12-21 22:11:11 +0200

    Slide animation

--- js/components/caseStudies/caseStudiesRender.js
+++ js/components/caseStudies/caseStudiesRender.js
@@ -7,8 +7,7 @@ class CaseStudiesRender {
         this.bubblesDOM = document.getElementsByClassName('bubble');
         this.activeBubbleIndex = 0;
         this.cardsBlockDOM = null;
-        this.showCards = NaN;
-        // this.defaultShow = 0;
+        this.showCards = 3;
 
         this.innerWidthDesk = window.matchMedia("(min-width: 1300px)"); //TIkrina ar ekrano dydis yra daugiau nei 1300px (Atsakymas true arba false)
         this.innerWidthLg = window.matchMedia("(min-width: 1080px) and (max-width: 1300px)"); //TIkrina ar ekrano dydis yra tarp 1080px ir 1300px (Atsakymas true arba false)
@@ -23,10 +22,7 @@ class CaseStudiesRender {
         if (!this.isValidSelector()) {
             console.error('There is no such selector');
             return;
-        }
-        this.cards()
-        
-        
+        }     
         this.render();
         this.addEvents();
     }
@@ -40,16 +36,57 @@ class CaseStudiesRender {
     }
 
     generateControls() {
-        const cardCount = this.data.length;
+
+        if(this.innerWidthDesk.matches){ //Tikrina kiek turime aprasytu korteliu ir skaiciuoja kiek parodyt navigacijos burbulu pvz(Turim 4 aprasytas korteles ir 3 korteles ant ekrano, reiskias reikia atvaizduot 2 burbulus pirmas burbulas trim pirmom kortelem o antras burbulas ketvirtai kortelei)
+            const cardCount = this.data.length;
         let bubbleHTML = '<div class="bubble active"></div>';
-        bubbleHTML += `<div class="bubble"></div>`.repeat(cardCount - 1);
+        bubbleHTML += `<div class="bubble"></div>`.repeat(cardCount - this.showCards );
         let HTML = `<div class="bubbles">${bubbleHTML}
         </div>`
         return HTML;
+        }
+        if(this.innerWidthLg.matches){
+            const cardCount = this.data.length;
+        let bubbleHTML = '<div class="bubble active"></div>';
+        bubbleHTML += `<div class="bubble"></div>`.repeat(cardCount - this.showCards );
+        let HTML = `<div class="bubbles">${bubbleHTML}
+        </div>`
+        return HTML;
+        }
+        if(this.innerWidthMd.matches){
+            const cardCount = this.data.length;
+        let bubbleHTML = '<div class="bubble active"></div>';
+        bubbleHTML += `<div class="bubble"></div>`.repeat(cardCount - 2 );
+        let HTML = `<div class="bubbles">${bubbleHTML}
+        </div>`
+        return HTML;
+        }
+        if(this.innerWidthSm.matches){
+            const cardCount = this.data.length;
+        let bubbleHTML = '<div class="bubble active"></div>';
+        bubbleHTML += `<div class="bubble"></div>`.repeat(cardCount - 2 );
+        let HTML = `<div class="bubbles">${bubbleHTML}
+        </div>`
+        return HTML;
+        }
+        if(this.innerWidthXs.matches){
+            const cardCount = this.data.length;
+        let bubbleHTML = '<div class="bubble active"></div>';
+        bubbleHTML += `<div class="bubble"></div>`.repeat(cardCount - 1 );
+        let HTML = `<div class="bubbles">${bubbleHTML}
+        </div>`
+        return HTML;
+        }
+        
+        
+
+
     }
     generate() {
 
-
+        if(this.isDataValid(this.data)) {
+            return
+        }
         if(this.innerWidthDesk.matches) { //tikrina ar aprasytas dydis atitinka ekrano dydi ir jeigu true paleidzia metoda
             let itemWidth = 0;
             let HTML ='';
@@ -159,7 +196,7 @@ class CaseStudiesRender {
         }
         
     render() {
-        if(!this.cards()) {
+        if(!this.cards(this.showCards)) {
             return
         }
         if (this.innerWidthDesk.matches) { //tikrina ar aprasytas dydis atitinka ekrano dydi ir jeigu true paleidzia metoda
@@ -180,7 +217,6 @@ class CaseStudiesRender {
 
         if( this.innerWidthMd.matches) {
             const marginLeft = 100 *this.clones;
-            console.log(marginLeft);
             const blockWidth = (this.data.length + 2 * this.clones) * 100;
             const HTML = `<div class="cards-block"style="width:${blockWidth}%; margin-left:-${marginLeft / 2}%"> ${this.generate()}</div>
             ${this.generateControls()}`
@@ -205,8 +241,8 @@ class CaseStudiesRender {
 
     clickBubble(bubbleIndex) {
 
-        if(this.innerWidthDesk.matches){ //tikrina ar aprasytas dydis atitinka ekrano dydi ir jeigu true paleidzia metoda
-            const scroll = -100 / this.showCards * (bubbleIndex + this.clones) + '%';
+        if(this.innerWidthDesk.matches){ //tikrina ar aprasytas dydis atitinka ekrano dydi ir jeigu true paleidzia metoda\
+        const scroll = -100 / this.showCards * (bubbleIndex + this.clones) + '%';
         this.cardsBlockDOM = this.DOM.querySelector('.cards-block');
         const bubble = this.bubblesDOM[bubbleIndex];
         this.cardsBlockDOM.style.marginLeft = scroll
@@ -268,22 +304,37 @@ class CaseStudiesRender {
 
     }
 
-    cards() {
+    cards(number) {
 
-        if(this.showCards > 3) {
-            console.error("You can't show more than 3 cards at the same time");
-            this.showCards = 3;
             
-        }
-        if(typeof this.showCards !== 'number') {
+        if(typeof number !== 'number') {
             console.error('Please enter number to show cards');
             return false;
         }  
-        if(isNaN(this.showCards)){
+        if(isNaN(number)){
             console.error('Please enter number to show cards');
             return false;
             
-        }    
+        } 
+        if(!Number.isInteger(number)){
+            console.error("To show cards please enter whole number");
+            
+            
+        }
+        if(number > 3) {
+            console.error("You can't show more than 3 cards at the same time");
+            this.showCards = 3;
+            return true
+        }  
+        else {
+            return true
+        }
+    }
+
+    isDataValid(data) {
+        if(!Array.isArray(data) || data.length === 0) {
+            console.error('Provided data is not array type or empty');
+        }
     }
 
 
--- js/data/caseStudiesData.js
+++ js/data/caseStudiesData.js
@@ -1,25 +1,26 @@
 const caseStudiesData = {
-    data: [{
-            img: "./img/case-study/mitech.jpg",
-            title: "Mitech - Smart Vision Technology",
-            description: "At Mitech, we have a holistic and integrated approach towards core modernization to experience technological evolution."
-        },
-        {
-            img: "./img/case-study/arden.jpg",
-            title: "Arden - Internal networking ",
-            description: "The prospects for a company operating experience technological in this new market are varied and exciting. The potential…"
-        },
-        {
-            img: "./img/case-study/aqua.jpg",
-            title: "Aqua - Research and Technology",
-            description: "The prospects for a company operating experience technological in this new market are varied and exciting. The potential…"
-        },
-        {
-            img: "./img/case-study/aqua.jpg",
-            title: "Aqua - Research and Technology",
-            description: "The prospects for a company operating experience technological in this new market are varied and exciting. The potential…"
-        }
+    data:[{
+        img: "./img/case-study/mitech.jpg",
+        title: "Mitech - Smart Vision Technology",
+        description: "At Mitech, we have a holistic and integrated approach towards core modernization to experience technological evolution."
+    },
+    {
+        img: "./img/case-study/arden.jpg",
+        title: "Arden - Internal networking ",
+        description: "The prospects for a company operating experience technological in this new market are varied and exciting. The potential…"
+    },
+    {
+        img: "./img/case-study/aqua.jpg",
+        title: "Aqua - Research and Technology",
+        description: "The prospects for a company operating experience technological in this new market are varied and exciting. The potential…"
+    },
+    {
+        img: "./img/case-study/arden.jpg",
+        title: "Arden - Internal networking ",
+        description: "The prospects for a company operating experience technological in this new market are varied and exciting. The potential…"
+    }
 
-    ]
+]
 }
+
 export { caseStudiesData }
