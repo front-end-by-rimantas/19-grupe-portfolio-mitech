bdfebfc1f5b874c8f7b3a4bee2d7b02941f07c54
Andrius Kairys <andrius.kairys.lt@gmail.com>
2020-12-23 04:46:49 +0200

    testimonial slide without teleport

--- js/components/testimonials/Testimonials.js
+++ js/components/testimonials/Testimonials.js
@@ -8,8 +8,15 @@ class Testimonials {
         this.cloneCount = 5; //how many clones
         this.cardView = 3; //how many testimonials visible
 
+        this.animationDuration = 600;
+
         this.testimcardsDOM = null;
         this.cardDOM = null;
+        this.bubblesDOM = null;
+
+        this.activeBubbleIndex = 0;
+
+        this.isAnimating = false;
 
         this.init();
     }
@@ -21,6 +28,7 @@ class Testimonials {
         this.cardViewCount();
         this.render();
         this.widthAdjust();
+        this.clickBubbleEvent();
     }
 
     isValidSelector() {
@@ -44,9 +52,9 @@ class Testimonials {
     }
 
     generateBubbles() {
-        let HTML = '<div class="dot active"></div>';
+        let HTML = '<div class="bubble active"></div>';
         for (let i = 1; i < this.originalCount; i++) {
-            HTML += `<div class="dot"></div>`;
+            HTML += `<div class="bubble"></div>`;
         }
         return HTML;
     }
@@ -112,7 +120,8 @@ class Testimonials {
 
         this.DOM.innerHTML = HTML;
         this.testimcardsDOM = document.querySelector('.testimcards');
-        this.cardDOM = document.querySelectorAll('.testimonialcard')
+        this.cardDOM = document.querySelectorAll('.testimonialcard');
+        this.bubblesDOMs = document.querySelectorAll('#testimonials .bubble');
     }
 
     //change width of elements depeneding on screen width
@@ -122,7 +131,7 @@ class Testimonials {
             const itemCount = this.originalCount + this.cloneCount;
             let containerWidth = 100 * itemCount / this.cardView;
             let itemWidth = 100 / this.cardView;
-            let positionAdjust = 1.5 - (this.cardView / 2);
+            let positionAdjust = 1.5 + this.activeBubbleIndex - (this.cardView / 2);
             let firstOriginalPosition = -containerWidth / itemCount * (this.generateAfterCloneArray().length + positionAdjust);
             this.testimcardsDOM.style.width = `${containerWidth}%`;
             this.testimcardsDOM.style.marginLeft = `${firstOriginalPosition}%`;
@@ -130,6 +139,33 @@ class Testimonials {
                 this.cardDOM[i].style.width = `${itemWidth}%`
             }
         })
+    }
+
+    clickBubble(bubbleIndex) {
+        for (let i = 0; i < this.bubblesDOMs.length; i++) {
+            const bubble = this.bubblesDOMs[i];
+            if (i != bubbleIndex) {
+                bubble.classList.remove('active')
+            }
+            if (i === bubbleIndex) {
+                bubble.classList.add('active')
+            }
+        }
+        this.activeBubbleIndex = bubbleIndex;
+        const itemCount = this.originalCount + this.cloneCount;
+        let containerWidth = 100 * itemCount / this.cardView;
+        let positionAdjust = 1.5 + this.activeBubbleIndex - (this.cardView / 2);
+        let firstOriginalPosition = -containerWidth / itemCount * (this.generateAfterCloneArray().length + positionAdjust);
+        this.testimcardsDOM.style.marginLeft = `${firstOriginalPosition}%`;
+    }
+
+    clickBubbleEvent() {
+        for (let i = 0; i < this.bubblesDOMs.length; i++) {
+            const bubble = this.bubblesDOMs[i];
+            bubble.addEventListener('click', () => {
+                this.clickBubble(i);
+            })
+        }
     }
 
 }
