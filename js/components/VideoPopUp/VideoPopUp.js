cc4402ab7e56b10f545805170a2c3514191a4229
martynas3113 <martynas3113@users.noreply.github.com>
2020-12-24 12:45:49 +0200

    Added comments nad fixed timer

--- js/components/VideoPopUp/VideoPopUp.js
+++ js/components/VideoPopUp/VideoPopUp.js
@@ -51,33 +51,51 @@ class VideoPopUp {
     addEvents(){
         const playButtonDOM = document.querySelector('.video-play > a')
         this.PlayDOM = playButtonDOM;
+        // Atidaro paslepta DIV kuomet paspaudziamas video play mygtukas
         this.PlayDOM.addEventListener('click', () => {
             this.DOM.classList.remove('hidden')
 
         })
         const closeDOM = document.querySelector('.toolbar > span .fa-times')
         this.closeVideo = closeDOM;
+        //uzdaro video langa kuomet paspaudziamas X
         this.closeVideo.addEventListener('click', ()=>{
             this.DOM.classList.add('hidden')
         })
-        const toolbar = document.querySelector('.pop-up > .toolbar')
-        this.toolBarDOM = toolbar;
-        addEventListener('mousemove',()=>{
-            this.toolBarDOM.classList.remove('hidden')
-            // setInterval(() => {
-            //     this.toolBarDOM.classList.add('hidden')
-            // }, 5000);
-
-        })
-        
+        // Isjungia video langa kuomet paspaustaqs mygtukas escape
         addEventListener('keyup',({ key })=> {
             if(key === 'Escape'){
                 this.DOM.classList.add('hidden')
                 
             }
         })
-    }
 
-}
+        const toolbar = document.querySelector('.pop-up > .toolbar')
+        this.toolBarDOM = toolbar;
+        addEventListener('mousemove', () => {
+            // nieko nedaro, kol neatidarytas video langas
+                if (this.DOM.classList.contains('hidden')) {
+                    return
+                } else {
+            // parodo ikonas vos tik atsidarius video langui ir sujudejus cursoriui
+                    if (toolbar.classList.contains('hidden')) {
+                        toolbar.classList.remove('hidden');
+                    } else {
+            // jei ikonos matomos inicijuoja timeri, kad jos dingtu uz 5 sekundziu
+                        const timer =
+                            setTimeout(() => {
+                                toolbar.classList.add('hidden')
+                            }, 5000)
+            // jei ikonos matomos, bet pele vis dar juda timeris cancel'inamas ir inicijuojamas is naujo del auksciau buvusio eventListenerio
+                        addEventListener('mousemove', ()=> {
+                            if (!toolbar.classList.contains('hidden')) {
+                                clearTimeout(timer);
+                            }
+                        })
+                    }
+                }
+            })
+        }
+    }
 
 export {VideoPopUp}
